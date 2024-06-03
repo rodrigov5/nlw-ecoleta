af90532c1ced89bf06f108cee9ec0ad127b4e75b
Rodrigo Veloso <57206601+rodrigov5@users.noreply.github.com>
2024-06-03 06:21:00 -0300

    backend da aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,12 @@
 import express from "express";
+import path from "path";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  response.json(["Rodrigo", "Rodrigues", "Veloso"]);
-});
+app.use(express.json());
+app.use(routes);
 
-app.post("/users", (request, response) => {
-  const user = {
-    name: "Rodrigo",
-    email: "rodrigovelosow@gmail.com",
-  };
-
-  return response.json(user);
-});
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333);
