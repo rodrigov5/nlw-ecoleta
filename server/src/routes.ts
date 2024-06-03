a317962dc715a0068d004f1caef100bbfe06fcfa
rodrigov <rodrigovelosow@gmail.com>
2024-06-03 11:41:51 -0300

    Rota de criação de pontos de coleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,50 @@
 import express from "express";
+import knex from "./database/connection";
 
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
+routes.get("/items", async (request, response) => {
+  const items = await knex('items').select('*');
+
+  const serializadItems = items.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`, 
+    };
+  });
+
+  return response.json(serializadItems);
 });
+
+routes.post("/points", async (request, response) => {
+  const {
+    name, email, whatsapp,
+    latitude, longitude, 
+    city, uf, items
+  } = request.body;
+
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx('points').insert({
+    image: 'image-fake',
+    name, email, whatsapp,
+    latitude, longitude, 
+    city, uf,
+  });
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map((item_id: Number) => {
+    return {
+      item_id,
+      point_id,
+    }
+  });
+
+  await trx('point_items').insert(pointItems);
+
+  return response.json({ success: true })
+})
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import path from "path";
 import routes from "./routes";
+import path from "path";
 
 const app = express();
 
